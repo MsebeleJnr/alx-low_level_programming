de6fab38cca986a7bfbd1faaa3ed1695fa1b03a5
MsebeleJnr <msebelegift@gmail.com>
2023-06-18 06:43:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print numbers of base 10'
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
+
