99b7107b6b77b7ab741e6c9b0608633e9e434f2c
MsebeleJnr <msebelegift@gmail.com>
2023-06-18 05:55:03 -0700

    ./3-print_alphabets | cat -e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+	char g;
+
+	char h;
+
+	g = 'a';
+	h = 'A';
+	while
+		(g <= 'z') {
+			putchar(g);
+			g++;
+
+
+		}
+	while
+		(h <= 'Z') {
+			putchar (h);
+			h++;
+
+
+		}
+	putchar('\n');
+	return (0);
+}
+
