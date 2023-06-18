a93bb8c0683057d2f702fac9d0ce7143d2fc516d
MsebeleJnr <msebelegift@gmail.com>
2023-06-18 05:44:30 -0700

    ./2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Everytime 0 (Done)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+
+			putchar(c);
+			c++;
+
+
+		}
+	putchar('\n');
+	return (0);
+}
